1fdee341d8cd3cdc67088be196580754bafe7215
sauravrawat99 <rawatsaurav145@gmail.com>
2026-04-07 11:06:34 +0530

    Add impotant middleware helmet morgan cors and more

--- server/server.js
+++ server/server.js
@@ -4,12 +4,30 @@ const express = require("express");
 const { Dbconnect, PORT } = require("./config/Db");
 const authRoutes = require("./routes/authRoutes");
 const ErrorMiddleware = require("./utils/ErrorMiddleware");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
 
 // app init
 const app = express();
 
-// middleware
+// sets around 14 security headers to protest against common vulnerabilities
+app.use(helmet());
+
+// middleware ==> functions that have access to the request and response objects
+//  and can modify them or perform actions before passing control to the next
+//  middleware or route handler. They are used for tasks like logging,
+//  authentication, error handling, etc.
+app.use(morgan("dev"));
+
+// Middleware to enable Cross-Origin Resource Sharing (CORS) and allow requests
+//  from different origins
+app.use(cors());
+
+// Middleware to parse incoming JSON payloads and make them available in req.body
 app.use(express.json());
+
+// middleware to handle routes related to authentication, such as login and registration
 app.use("/api/auth", authRoutes);
 
 // test route
@@ -19,7 +37,7 @@ app.get("/", (req, res) => {
 
 Dbconnect();
 
-//Error handler
+// Middleware to handle errors that occur during request processing and send appropriate
 app.use(ErrorMiddleware);
 
 app.listen(PORT, () => {
