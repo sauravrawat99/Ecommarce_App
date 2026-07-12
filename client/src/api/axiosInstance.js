30a3355c02bac3847e9290bb0db5cb960f1d4607
sauravrawat99 <rawatsaurav145@gmail.com>
2026-07-12 14:40:38 +0530

    add production API URL

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://ecommarce-app-1.onrender.com",
   timeout: 10000,
   withCredentials: true,
   headers: {
