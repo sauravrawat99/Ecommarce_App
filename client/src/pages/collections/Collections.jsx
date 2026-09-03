3ae71253da2a5feaa54b7dec1504eb25434f0cd8
sauravrawat99 <rawatsaurav145@gmail.com>
2026-09-03 17:12:51 +0530

    style: fix product grid columns and responsive layout

--- client/src/pages/collections/Collections.jsx
+++ client/src/pages/collections/Collections.jsx
@@ -5,8 +5,6 @@ import { getAllCollection } from "../../redux/slices/collectionsSlice";
 
 const Collections = () => {
   const dispatch = useDispatch();
-  // 👈 "collection" slice name se match karna zaroori hai (jo createSlice mein diya tha),
-  // aur property lowercase "collections" honi chahiye (jo initialState mein defined hai)
   const { collections, loading, error } = useSelector(
     (state) => state.collection,
   );
@@ -21,9 +19,9 @@ const Collections = () => {
   if (error) return <div>Something went wrong: {error}</div>;
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="grid grid-cols-2 md:grid-cols-4 gap-6 px-6 md:px-16 py-8">
       {collections.map((collection) => (
-        <CollectionCard key={collection._id} collection={collection} />
+        <CollectionCard key={collection.slug} collection={collection} />
       ))}
     </div>
   );
